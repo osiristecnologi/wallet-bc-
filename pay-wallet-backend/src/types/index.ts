0057e6e3cd66aead4cd4f85b089ed488bb04da54
Osiris technology <107819115+osiristecnologi@users.noreply.github.com>
2026-06-01 13:45:52 -0300

    Add JwtPayload and AuthRequest interfaces

--- pay-wallet-backend/src/types/index.ts
+++ pay-wallet-backend/src/types/index.ts
@@ -1,1 +1,11 @@
+import { Request } from 'express';
 
+export interface JwtPayload {
+  id: string;
+  role: 'USER' | 'ADMIN';
+  entityType: 'user' | 'admin';
+}
+
+export interface AuthRequest extends Request {
+  user: JwtPayload;
+}
